da278325cb805b779905591b0567f6b9b135bc8b
Manav Manav <manav2102005@gmail.com>
2026-01-25 04:46:59 -0500

    Update client.js

--- wanderlust-frontend/src/api/client.js
+++ wanderlust-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",          // uses Vite proxy
+  baseURL: import.meta.env.VITE_API_BASE_URL,         // uses Vite proxy
   withCredentials: true,    // sends auth cookies
 });
 
